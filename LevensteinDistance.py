6eb9a3d82f9c0261642d26ed8825b53143b3cfc0
srujanmurthy <srujanmurthy2006@gmail.com>
2023-04-13 23:53:35 +0000

    Uses an algebraic method to evaluate textual difference in words

---
+++ LevensteinDistance.py
@@ -0,0 +1,15 @@
+def levenshtein_distance(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(m + 1):
+        dp[i][0] = i
+    for j in range(n + 1):
+        dp[0][j] = j
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+    return dp[m][n]
