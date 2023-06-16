23065be00de9e81e290a6ed15e2336461bb9fd9d
coder-sys <71105356+coder-sys@users.noreply.github.com>
2023-06-15 23:18:03 -0500

    Update test_sapphire.py

--- test_sapphire.py
+++ test_sapphire.py
@@ -1,3 +1,4 @@
+"""
 import unittest
 from RigorEvaluation.TextScoring import InformationalRetrievalRank
 from RigorEvaluation.TFIDFEvaluation import TFIDFEvaluation
@@ -16,5 +17,37 @@ class test_sapphire(unittest.TestCase):
         self.assertLessEqual(rank_score, 100.0)
 
 
+if __name__ == "__main__":
+    unittest.main()
+"""
+
+import unittest
+from unittest.mock import patch
+from RigorEvaluation.TextScoring import InformationalRetrievalRank
+from RigorEvaluation.TFIDFEvaluation import TFIDFEvaluation
+from TokenSatisfaction.KeywordQualification import KeywordQualification
+import nltk
+from sapphire import Sapphire
+import httpretty
+nltk.download("punkt")
+
+
+class test_sapphire(unittest.TestCase):
+    def test_sapphire(self):
+        videoID = "--qKOhdgJAs"
+
+        # Mock the Sapphire function to return a desired value
+        mock_json_response = """{"kind":"youtube#videoListResponse","items":[{"kind":"youtube#video","etag":"exampleETag","id":"example_videoID","contentDetails":{"duration":"PT2H13M17S"}}]}"""
+        videoID = "example_videoID"
+        api_key = "example_api_key"
+        expected_result = 69.6828989690041
+        httpretty.enable()
+        httpretty.register_uri(
+            httpretty.GET,
+            "https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=example_videoID&key=example_api_key",
+            body=mock_json_response,
+        )
+        actual_result = Sapphire("bqu6BquVi2M")
+        self.assertEqual(expected_result, actual_result)
 if __name__ == "__main__":
     unittest.main()
