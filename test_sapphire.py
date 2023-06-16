19ea0c2fdf40260971d30db4388725833b930006
coder-sys <71105356+coder-sys@users.noreply.github.com>
2023-06-16 16:44:44 -0500

    Update test_sapphire.py

--- test_sapphire.py
+++ test_sapphire.py
@@ -32,22 +32,29 @@ import httpretty
 nltk.download("punkt")
 
 
+import unittest
+import httpretty
+
 class test_sapphire(unittest.TestCase):
     def test_sapphire(self):
         videoID = "--qKOhdgJAs"
 
         # Mock the Sapphire function to return a desired value
+
         mock_json_response = """{"kind":"youtube#videoListResponse","items":[{"kind":"youtube#video","etag":"exampleETag","id":"example_videoID","contentDetails":{"duration":"PT2H13M17S"}}]}"""
-        videoID = "example_videoID"
         api_key = "example_api_key"
-        expected_result = 69.6828989690041
+        expected_result = 94.10661404911535
+
         httpretty.enable()
         httpretty.register_uri(
             httpretty.GET,
             "https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=example_videoID&key=example_api_key",
             body=mock_json_response,
         )
-        actual_result = Sapphire("bqu6BquVi2M")
+
+        actual_result = Sapphire(videoID)
+
         self.assertEqual(expected_result, actual_result)
+
 if __name__ == "__main__":
     unittest.main()
