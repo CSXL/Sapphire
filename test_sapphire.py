e834fe6b8db6b4dc57f7f74ed0a39a55f5936c79
coder-sys <71105356+coder-sys@users.noreply.github.com>
2023-06-19 16:04:17 -0500

    Update test_sapphire.py

--- test_sapphire.py
+++ test_sapphire.py
@@ -1,11 +1,27 @@
-from unittest import mock
 import unittest
+
+import httpretty
+
 from sapphire import Sapphire
-import os.path
-class testSapphire(unittest.TestCase):
+
+
+class TestSapphire(unittest.TestCase):
     def test_calculate_rank_score(self):
-        patcher = mock.patch.object(Sapphire, 'calculate_rank_score')
-        mock_calculate_rank_score = patcher.start()
-        mock_calculate_rank_score.return_value = 69.6828989690041
-        assert mock_calculate_rank_score.return_value ==  69.6828989690041
-        patcher.stop()
+
+        # Mock JSON response
+        # Adapted from request made to 'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=ix5jPkxsr7M&key=[YOUR_API_KEY]
+        # on 4/26/2023.
+        mock_json_response = """[{"text":"example_text","start":"example_start","duration":"example_duration"}]"""
+        videoID = "example_videoID"
+        api_key = "example_api_key"
+        expected_score = 69.6828989690041
+        httpretty.enable()
+        httpretty.register_uri(
+            httpretty.GET,
+            "https://youtube.googleapis.com/youtube/v3/captions?key=[example_api_key]&videoId=[example_videoID]",
+            body=mock_json_response,
+        )
+        actual_duration = Sapphire().calculate_rank_score(videoID)
+        self.assertEqual(expected_duration, actual_duration)
+
+        
