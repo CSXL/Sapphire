436327cf5f083b12f63feb6428d02856ec8c6f90
coder-sys <71105356+coder-sys@users.noreply.github.com>
2023-06-19 16:10:31 -0500

    Update sapphire.py

--- sapphire.py
+++ sapphire.py
@@ -10,38 +10,39 @@ from TokenSatisfaction.KeywordQualification import KeywordQualification
 nltk.download("punkt")
 
 
-def Sapphire(videoID: str) -> float:
-    """
-    To get the rank score of the YouTube video(videoID) using the Sapphire algorithm, follow these steps.
+class Sapphire:
+    @staticmethod
+    def calculate_rank_score(videoID: str) -> float:
+        """
+        Calculates the rank score of the YouTube video using the Sapphire algorithm.
 
-    Args:
-    ----
-    - videoID (str): The unique identification of a YouTube video.
+        Args:
+        - videoID (str): The unique identification of a YouTube video.
 
-    Returns:
-    -------
-    - float: The rank score of the video.
+        Returns:
+        - float: The rank score of the video.
+        """
+        # Initialize the evaluation object
+        ir_evaluator = TFIDFEvaluation(videoID)
 
-    """
+        # Perform text pre-processing
+        ir_evaluator.lower_case_conversion()
+        ir_evaluator.evaluate_term_freq()
+        ir_evaluator.evaluate_inverse_doc_freq()
+        ir_evaluator.initialize_json_scores()
+        ir_evaluator.strip_non_ascii()
+        ir_evaluator.filter_words()
 
-    # Initialize the evaluation object
-    ir_evaluator = TFIDFEvaluation(videoID)
+        # Perform keyword qualification
+        ir_evaluator.fallback_evaluation()
+        k_qualifier = KeywordQualification(
+            ir_evaluator.tokens, ir_evaluator.tf_idf_score
+        )
+        (freq_dist, qualification, qualified_terms) = k_qualifier.show()
 
-    # Perform text pre-processing
-    ir_evaluator.lower_case_conversion()
-    ir_evaluator.evaluate_term_freq()
-    ir_evaluator.evaluate_inverse_doc_freq()
-    ir_evaluator.initialize_json_scores()
-    ir_evaluator.strip_non_ascii()
-    ir_evaluator.filter_words()
+        # Calculate rank score
+        rank_score = InformationalRetrievalRank(
+            qualified_terms, freq_dist, qualification
+        )
 
-    # Perform keyword qualification
-    ir_evaluator.fallback_evaluation()
-    k_qualifier = KeywordQualification(ir_evaluator.tokens, ir_evaluator.tf_idf_score)
-    (freq_dist, qualification, qualified_terms) = k_qualifier.show()
-
-    # Calculate rank score
-    rank_score = InformationalRetrievalRank(qualified_terms, freq_dist, qualification)
-
-    return float(rank_score)
-print(Sapphire("bqu6BquVi2M"))
+        return float(rank_score)
