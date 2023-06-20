a25b37948b8d5412298763d307f5eb8b087ae55f
ecsbeats <adamb@csxlabs.org>
2023-06-20 16:04:30 -0700

    add html mocking to unittest

--- test_sapphire.py
+++ test_sapphire.py
@@ -18,10 +18,26 @@ class TestSapphire(unittest.TestCase):
         httpretty.enable()
         httpretty.register_uri(
             httpretty.GET,
-            "https://youtube.googleapis.com/youtube/v3/captions?key=[example_api_key]&videoId=[example_videoID]",
+            f"https://youtube.googleapis.com/youtube/v3/captions?key={videoID}&videoId={api_key}",
             body=mock_json_response,
         )
+        with open("tests/sample_data/example_youtube_video.html", "r") as f:
+            mock_youtube_html_response = f.read()
+        with open("tests/sample_data/example_xml_youtube_transcript.xml", "r") as f:
+            mock_youtube_xml_response = f.read()
+        httpretty.register_uri(
+            httpretty.GET,
+            f"https://www.youtube.com/watch?v={videoID}",
+            body=mock_youtube_html_response,
+        )
+        # This is the request that is made to get the final transcript
+        httpretty.register_uri(
+            httpretty.GET,
+            "https://www.youtube.com/api/timedtext?v=T9xsTO6ujqM&caps=asr&opi=112496729&xoaf=5&hl=en&ip=0.0.0.0&ipbits=0&expire=1687325993&sparams=ip,ipbits,expire,v,caps,opi,xoaf&signature=82EC42B531F868F24D3EF4C6765BFA55A735C121.851DCD4FDCD046D8793E98EFEDC7F451B8156CFE&key=yt8&kind=asr&lang=en",
+            body=mock_youtube_xml_response,
+        )
+
         actual_duration = Sapphire().calculate_rank_score(videoID)
-        self.assertEqual(expected_duration, actual_duration)
+        self.assertEqual(expected_score, actual_duration)
 
         
